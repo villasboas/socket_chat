4da2796497930827964d35f62bc5dfae5b40817e
villasboas <gu.boas13@gmail.com>
2016-09-10 12:31:24 -0300

    Getting start node platform

--- index.js
+++ index.js
@@ -1,41 +1,20 @@
-//configurações iniciais
 var express = require('express');
-var app  	= express();
-var http 	= require('http').Server(app);
-var io   	= require('socket.io')(http);
-var path 	= require('path');
+var app = express();
 
-/*configura o express
-app.engine('.html', require('ejs').__express);
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
-//define o caminho de arquivos publicos
-app.use(express.static(path.join(__dirname, 'public')));
-
-//pagina principal do site
-app.get('/?', function(req, res){
-	res.sendFile(__dirname+'/views/index.html');
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-chama o evento connecto no objeto io
-io.on('connection', function(socket){
-	
-	//ações quando o socket for conectado	
-	console.log('A user connected');
-
-	//ações quando o socket for desconectado
-	socket.on('disconnect', function(){
-		console.log('A user disconnected');
-	})
-
-	//recebe as mensagens
-	socket.on('chat message', function(msg){
-		socket.broadcast.emit('chat message', msg);
-	})
-})*/
-
-//faz o server escutar a porta 3000
-http.listen(8080, function(){
-	console.log('Listening on port *:8080');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
+
